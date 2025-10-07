6ee4f1060c97ff72528bd27bf5ee007784c35e2e
jlopez1137 <lopejo@iu.edu>
2025-10-07 11:00:10 -0400

    feat: add headshot asset and CDN mapping; update pages

--- js/cdn-config.js
+++ js/cdn-config.js
@@ -1,7 +1,7 @@
 // CDN image mapping. Replace the example URLs with your cloud/CDN links.
 // Supported keys should match data-cdn-key attributes in your HTML.
 window.CDN_IMAGES = {
-    headshot: 'https://cdn.jsdelivr.net/gh/jlopez1137/personal-website@main/images/headshot.jpg'
+    headshot: 'https://cdn.jsdelivr.net/gh/jlopez1137/personal-website@main/images/headshot.jpg?v=1'
 };
 
 
